964e64638f72165bfc35ea3268c354ef8e64d962
Nicnit <nicpionit@gmail.com>
2026-08-28 18:53:05 -0400

    added continue session manager, removed swapfiles in favor of it

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.swapfile = false -- everything swapfiles would do are already covered by niba/continue session manager
+vim.opt.undofile = true -- Keep undo history after closing
